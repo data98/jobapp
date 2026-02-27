160d8d403ed458e847292a530c630463657fdfb0
data98 <davitgoderdzishvili1998@gmail.com>
2026-02-27 21:55:17 +0400

    added google auth feature with translations

--- lib/auth.ts
+++ lib/auth.ts
@@ -9,6 +9,14 @@ export const auth = betterAuth({
     enabled: true,
     requireEmailVerification: false,
   },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      accessType: 'offline',
+      prompt: 'select_account consent',
+    },
+  },
   trustedOrigins: [process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000'],
 });
 
